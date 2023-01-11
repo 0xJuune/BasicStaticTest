d6674ef7331937b2f108d953089d719cb81d3cdf
Jonathan <jlbenfield.cccmc@gmail.com>
2023-01-10 22:01:18 -0500

    add TailwindCSS and add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Navbar from './Navbar';
 
 const App = () => {
     return (
-        <div>
+        <div className='h-screen bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r'>
             <Navbar />
-            <div>
-                <h1>This is a test of Netlify</h1>
+            <div className='flex flex-col justify-center items-center text-gray-200'>
+                <h1 className='text-3xl font-semibold'>This is a test of Netlify</h1>
                 <p>Just using this as a way to spin up and host for the first time.</p>
-                <button className='button'>BIG OL BUTTON</button>
+                <button className=''>BIG OL BUTTON</button>
                 <div className='output'>0</div>
             </div>
         </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Menu } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 
 const Navbar = () => {
     return (
-        <div className='Navbar'>
+        <div className='flex h-10 justify-end items-center p-4 bg-black bg-opacity-20 border-b border-white border-opacity-20'>
             <Menu>
-                <Menu.Button>Dropdown</Menu.Button>
-                <Menu.Items>
-                    <Menu.Item as="div">1</Menu.Item>
-                    <Menu.Item as="div">2</Menu.Item>
-                    <Menu.Item as="div">3</Menu.Item>
-                    <Menu.Item as="div">4</Menu.Item>
-                    <Menu.Item as="div" disabled>This is descriptive bottom text</Menu.Item>
-                </Menu.Items>
+                <Menu.Button className='text-gray-200 font-medium bg-black bg-opacity-30 p-1 border border-white border-opacity-20 rounded-md'>Dropdown</Menu.Button>
+                    <Transition 
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0">
+                        <Menu.Items>
+                            <Menu.Item as="div">1</Menu.Item>
+                            <Menu.Item as="div">2</Menu.Item>
+                            <Menu.Item as="div">3</Menu.Item>
+                            <Menu.Item as="div">4</Menu.Item>
+                            <Menu.Item as="div" disabled>This is descriptive bottom text</Menu.Item>
+                        </Menu.Items>
+                    </Transition>  
             </Menu>
         </div>
     )
